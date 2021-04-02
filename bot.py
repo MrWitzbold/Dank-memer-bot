f6e110a90fdb96b594bb4e07888b472e17627140
JUKjacker <80601981+MrWitzbold@users.noreply.github.com>
2021-04-02 02:14:46 -0300

    Made it less detectable
    The cat and mouse chase has begun...

--- bot.py
+++ bot.py
@@ -10,16 +10,20 @@ async def on_ready():
     print("ready")
 @client.event
 async def on_message(message):
+    commands_list = ["pls beg", "pls search", "pls hl", "pls trivia", "pls hunt", "pls fish", "pls pm", "pls use padlock"]
 
-    botter_id = "664274363243036682"
+    commmands_that_require_items = False
+    botter_id = "820162513873272833"
 
     if str(message.author.id) == "270904126974590976":
         print(message.content)
     if "Where do you want to search?" in str(message.content) and str("<@" + botter_id + ">") in str(message.content):
+        await asyncio.sleep(1.5)
         item = str(message.content).split(",")[1].replace(",", "").replace("`", "")
         await message.channel.send(item)
 
     if "type `" in str(message.content).lower() and message.author.id == "270904126974590976":
+        await asyncio.sleep(1.5)
         word = str(message.content).lower().split("type `")[1].replace("`", "")
         await asyncio.sleep(1)
         await message.channel.send(word)
@@ -32,7 +36,7 @@ async def on_message(message):
             found_highlow = True
             embed_with_hint = str(embed.to_dict())
     if found_highlow:
-        time.sleep(1)
+        await asyncio.sleep(1.5)
         number = str(embed_with_hint.split("Your hint is ")[1].split(".")[0]).replace("*", "")
         print(number)
         if int(number) > 50:
@@ -45,7 +49,7 @@ async def on_message(message):
     found_trivia = False
     trivia_embed = message.embeds
     for embed in embeds:
-        await asyncio.sleep(1)
+        await asyncio.sleep(1.5)
         if "trivia" in str(embed.to_dict()):
             choice = random.randint(0, 3)
             if choice == 0:
@@ -57,29 +61,51 @@ async def on_message(message):
             if choice == 3:
                 await message.channel.send("d")
     if "bank robbery" in str(message.content) and str(client.user.name) in str(message.content):
+        await asyncio.sleep(1.5)
         await message.channel.send("pls use cell")
         await asyncio.sleep(2)
         await message.channel.send("p")
     if message.content == ";start" and str(message.author.id) == "664274363243036682":
         while True:
-            await message.channel.send("pls beg")
-            await asyncio.sleep(1)
-            await message.channel.send("pls hunt")
-            await asyncio.sleep(1)
-            await message.channel.send("pls fish")
-            await asyncio.sleep(1)
-            await message.channel.send("pls pm")
-            await asyncio.sleep(2)
-            await message.channel.send("i")
-            await asyncio.sleep(1)
-            await message.channel.send("pls use padlock")
-            await asyncio.sleep(1)
-            await message.channel.send("pls search")
-            await asyncio.sleep(2)
-            await message.channel.send("pls highlow")
-            await asyncio.sleep(2)
-            await message.channel.send("pls trivia")
-            await asyncio.sleep(36)
+            sent_commands = ""
+            if commmands_that_require_items is False:
+                for i in range(0, 3):
+                    random_command = random.randint(0, 3)
+                    same_command = False
+                    if str(random_command) not in sent_commands:
+                        await message.channel.send(commands_list[random_command])
+                    else:
+                        same_command = True
+                        while same_command:
+                            random_command = random.randint(0, 3)
+                            if str(random_command) not in sent_commands:
+                                same_command = False
+                                await message.channel.send(commands_list[random_command])
+                    await asyncio.sleep(1 + random.randint(0, 1))
+                    sent_commands += str(random_command) + ";"
+                    print(sent_commands)
+            if commmands_that_require_items:
+                for i in range(0, len(commands_list)):
+                    random_command = random.randint(0, len(commands_list))
+                    same_command = False
+                    if str(random_command) not in sent_commands:
+                        await message.channel.send(commands_list[random_command])
+                    else:
+                        same_command = True
+                        while same_command:
+                            random_command = random.randint(0, len(commands_list))
+                            if str(random_command) not in sent_commands:
+                                same_command = False
+                                await message.channel.send(commands_list[random_command])
+                    if random_command == 1 or random_command == 2 or random_command == 3 or random_command == 6:
+                        await asyncio.sleep(2)
+                    else:
+                        await asyncio.sleep(1 + random.randint(0, 2))
+                    sent_commands += str(random_command) + ";"
+                    print(sent_commands)
+            await asyncio.sleep(35)
+
+
 
 
 client.run("nah", bot = False)
